1bf080c17b0fcf4e491a9e9614d5e0dd183933dd
Julen <julenpuertas@hotmail.com>
2019-02-12 20:02:20 +0100

    no message

--- hello-world/Main.cpp
+++ hello-world/Main.cpp
@@ -8,10 +8,49 @@
 #include "GameObject.h"
 #include "Color.h"
 #include <random>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+static void windowwwwww()
+{
+	GLFWwindow* window;
+
+	/* Initialize the library */
+	if (!glfwInit())
+		return;
+
+	/* Create a windowed mode window and its OpenGL context */
+	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+	if (!window)
+	{
+		glfwTerminate();
+		return;
+	}
+
+	/* Make the window's context current */
+	glfwMakeContextCurrent(window);
+
+	/* Loop until the user closes the window */
+	while (!glfwWindowShouldClose(window))
+	{
+		/* Render here */
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		///* Swap front and back buffers */
+		glfwSwapBuffers(window);
+
+		///* Poll for and process events */
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+}
 
 int main(int argc, char* argv[])
 {
 	using namespace Engine;
+
+
 
 	return 0;
 }
